d1af349ccd560cae0b7d5bd0a4a096c849387750
lindylouwerse <98382885+lindylouwerse@users.noreply.github.com>
2022-02-15 13:55:39 +0100

    Add files via upload

--- Operators/Operators.js
+++ Operators/Operators.js
@@ -10,14 +10,29 @@ numOfDaysUntilWeekend -= 1;
 3. Tell what it does 
 */
 
+let numOfDaysUntilWeekend = 2;
+console.log(numOfDaysUntilWeekend);
+numOfDaysUntilWeekend -= 1;
+console.log(numOfDaysUntilWeekend);
+//3. Show what is, print out. Change what is, print out. 
+
 //assignemnt 2
 //Addition to numbers
+let x = 5;
+x += 10;
+console.log(x);
 
 //assignemnt 3
 //Subtraction to numbers
+let x = 10;
+x -= 5;
+console.log(x);
 
 //assignemnt 4
 //write two sentences in two variables and connect them together 
+let x = 1;
+let y = 2;
+let z = x + y;
 
 //assignemnt 5
 //write two variables, one number and one string. add them together
@@ -26,29 +41,41 @@ numOfDaysUntilWeekend -= 1;
 //write the answears in the in a comment
 let r = 15;
 let s = 5;
-let t = r + s;
-let u = r - s;
-let v = t / u;
-let x = r * v;
-let y = s % v;
+let t = r + s; // 20
+let u = r - s; // 10
+let v = t / u; // 2
+let x = r * v; // 20
+let y = s % v; // 1
 
 //assignemnt 7
 //Why does it not write out? 
 let r = 34;
 let c = 1;
-let x = r - 32 + c;
-let y = r++ * 4; 
-if(x == 30 || y == 2 ){
+let x = r - 32 + c; // 3
+let y = r++ * 4; //16
+if(x == 30 || y == 2 ){ // they are both false
  console.log("yeah you got it");
 }
 //change a number in a variable, so it print out the text
-
+let r = 34;
+let c = 1;
+let x = r - 32 + c;
+let y = r++ * 4; 
+if(x !== 30 || y == 2 ){
+ console.log("yeah you got it");
+}
 
 //assignemnt 8
 //make two vaiables. One variable must contain a number and the other one must contain a prompt, 
 //where the user can enter a number. Match the two number and print out the answear
 //TIP: search prompt
-
+let a = 10;
+let result = prompt("give a number", default); 
+let x = a + result;
+let y = a - result; //if prompt is 5 then...
+if(x == 15 && y == 5){
+    console.log("Yay")
+}
 
 //assignment 9
 //write these numbers in the console and write the output. Explain you thing this happens
